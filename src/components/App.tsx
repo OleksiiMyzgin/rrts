b99211a881fff513cf878dcddb6eea10f945a850
Alexey <mr.oleksii5@gmail.com>
2019-10-26 09:47:06 +0300

    wiring up deleteToDo action

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from "react-redux";
-import { Todo, fetchTodos} from '../actions';
+import { Todo, fetchTodos, deleteTodo} from '../actions';
 import { StoreState} from '../reducers';
 
 interface AppProps {
     todos: Todo[];
-    fetchTodos(): any;
+    fetchTodos: Function;
+    deleteTodo: typeof deleteTodo;
 }
 class _App extends React.Component<AppProps> {
     onButtonClick = (): void => {
@@ -15,10 +16,18 @@ class _App extends React.Component<AppProps> {
     renderList = (): JSX.Element[] => {
         return(
             this.props.todos.map((todo: Todo) => {
-                return <div key={todo.id}>{todo.title}</div>
+                return (
+                    <div onClick={() => this.onTodoClick(todo.id)} key={todo.id}>
+                        {todo.title}
+                    </div>
+                );
             })
         );
     }
+
+    onTodoClick = (id: number): void => {
+        this.props.deleteTodo(id);
+    };
 
     render() {
         return (
@@ -34,4 +43,4 @@ const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
     return { todos }
 }
 
-export const App = connect(mapStateToProps, { fetchTodos })(_App);
+export const App = connect(mapStateToProps, { fetchTodos, deleteTodo })(_App);
